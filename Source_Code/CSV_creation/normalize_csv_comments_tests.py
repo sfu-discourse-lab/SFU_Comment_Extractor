c2efbf5bd396256f2f44e2330383e1b6c16284fb
vkolhatk <vkolhatk@sfu.ca>
2018-01-05 15:07:50 -0800

    Changing test cases for simplified normalization

--- Source_Code/CSV_creation/normalize_csv_comments_tests.py
+++ Source_Code/CSV_creation/normalize_csv_comments_tests.py
@@ -1,32 +1,46 @@
 import unittest
 import normalize_csv_comments
 from normalize_csv_comments import word_segmentation
-from normalize_csv_comments import read_dictionary
+from normalize_csv_comments import normalize
 
 class TestSegmenter(unittest.TestCase):
 
     def setUp(self):
-        self.dictionary = read_dictionary()
+        pass
 
-    #def test_punctuation(self):
-    #    input_text = r"Give your husband a big big kiss for me.But don't comehere."
-    #    expected = r"Give your husband a big big kiss for me. But don't come here."
-    #    self.assertEqual( word_segmentation(input_text), expected)
 
-    def test_simple_period(self):
+    def test_normalization(self):
         '''
         :return:
          This is a simple test case where space is missing after the sentence ending period.'
         '''
-        print('Simple test case where space is missing after the sentence ending period.')
+        print('Running simple test cases where space is missing after the sentence ending period.')
+        input_text = r'tired of having their paychequaes plundered is another'
+        expected = r'tired of having their paychequaes plundered is another'
+        self.assertEqual(normalize(input_text), expected)
 
         input_text = r"Rich,Funny you should mention the Roman Empire.That reminds me - Jesus Hippy was crucified for your skins."
         expected = r"Rich, Funny you should mention the Roman Empire. That reminds me - Jesus Hippy was crucified for your skins."
-        self.assertEqual(word_segmentation(input_text, self.dictionary), expected)
+        self.assertEqual(normalize(input_text), expected)
 
         input_text = r"They gone from the Nazi youth to a participant in the Dirty War.Lord help us."
         expected = r"They gone from the Nazi youth to a participant in the Dirty War. Lord help us."
-        self.assertEqual(word_segmentation(input_text, self.dictionary), expected)
+        self.assertEqual(normalize(input_text), expected)
+
+        input_text = r'''Why should temporary workers get paid more??How about Tim Horton's and all the rest of the companies rushing to hire all of these temporary foreign workers hire CANADIANS and PAY THEM A LIVING WAGE?HOW ABOUT THAT, HUH?This whole temp worker incentive is a drive the labour force to the bottom, nothing more.The government should be much tougher on hiring practices with these companies and instead build incentives for young Canadians, and especially in apprenticeship programs.'''
+        expected = r'''Why should temporary workers get paid more?? How about Tim Horton's and all the rest of the companies rushing to hire all of these temporary foreign workers hire CANADIANS and PAY THEM A LIVING WAGE? HOW ABOUT THAT, HUH? This whole temp worker incentive is a drive the labour force to the bottom, nothing more. The government should be much tougher on hiring practices with these companies and instead build incentives for young Canadians, and especially in apprenticeship programs.'''
+        self.assertEqual(normalize(input_text), expected)
+
+        # Make sure that the code is retaining newly created or mispelled words (e.g., Fibulously)
+        input_text = r'Fibulously good...'
+        expected = r'Fibulously good...'
+        self.assertEqual(normalize(input_text), expected)
+
+        input_text = r'Plagiarists are barnacles on the state of journalism.'
+        expected = r'Plagiarists are barnacles on the state of journalism.'
+        self.assertEqual(normalize(input_text), expected)
+        r'Is it your view that we should just sack these lay about s --'
+        r'Is it your view that we should just sack these layabouts -- '
 
 if __name__ == '__main__':
     unittest.main()
